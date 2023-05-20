62f4fe3eeb4b5b5f2ac766ff3c31ced90e097cbe
Nasruddinkhan <nasruddinkhan44@gmail.com>
2023-05-20 15:07:22 +0300

    add the kafka common consumer and producer

---
+++ infrastructure/kafka/kafka-producer/src/main/java/in/mypractice/foot/ordering/system/kafka/producer/exception/KafkaProducerException.java
@@ -0,0 +1,8 @@
+package in.mypractice.foot.ordering.system.kafka.producer.exception;
+
+public class KafkaProducerException extends RuntimeException {
+
+    public KafkaProducerException(String message) {
+        super(message);
+    }
+}
